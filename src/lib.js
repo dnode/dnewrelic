8adb8b4735ca8c2af5b2e4bc8cd4fa41bf6555e3
Christoph Herrmann <developer@dragonprojects.de>
2015-12-11 00:41:38 +0100

    Use the application name of the package.json as default

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,8 @@
 'use strict';
 
 if (process.env.NEW_RELIC_LICENSE_KEY) {
-  process.env.NEW_RELIC_APP_NAME = process.env.NEW_RELIC_APP_NAME || 'app';
+  process.env.NEW_RELIC_APP_NAME = process.env.NEW_RELIC_APP_NAME
+      || require(process.cwd() + '/package.json').name;
   process.env.NEW_RELIC_LOG = process.env.NEW_RELIC_LOG || 'stdout';
   require('newrelic');
 }
